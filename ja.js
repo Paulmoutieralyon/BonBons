505e118d35402b17758b00ca2c75f8714c819bb3
Paul Moutier[LYON] <moutier.paul@gmail.com>
2018-09-04 00:26:50 +0200

    Bonbons

---
+++ ja.js
@@ -0,0 +1,19 @@
+var cagnotte = Math.floor (13);
+var cout = Math.floor (3);
+
+
+
+
+
+if(cout>cagnotte){
+    console.log("Tu ne peut pas depenser plus ");
+}
+else if (cout<=cagnotte){
+    console.log(" Enjoy, Voila le nombre que tu peux acheter !")
+    
+}
+
+console.log(cagnotte/cout);
+
+
+
